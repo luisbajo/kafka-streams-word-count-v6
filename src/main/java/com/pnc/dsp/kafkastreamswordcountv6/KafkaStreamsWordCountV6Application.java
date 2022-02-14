a8adf1461fe541b51e3b968cc0e65f92d407641d
Luis Bajo <luis.bajo@hotmail.com>
2022-02-14 10:16:52 -0600

    First commit.

---
+++ src/main/java/com/pnc/dsp/kafkastreamswordcountv6/KafkaStreamsWordCountV6Application.java
@@ -0,0 +1,13 @@
+package com.pnc.dsp.kafkastreamswordcountv6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsWordCountV6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsWordCountV6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pnc/dsp/kafkastreamswordcountv6/KafkaStreamsWordCountV6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnc.dsp.kafkastreamswordcountv6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsWordCountV6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
